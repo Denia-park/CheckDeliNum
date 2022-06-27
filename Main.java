b690d996e46105e85d4c8b585edf9d7784802a23
Hyunggi <phg9587@naver.com>
2022-06-27 20:59:29 +0900

    edit : 출처 및 참조 주소 추가

--- Main.java
+++ Main.java
@@ -7,5 +7,9 @@ public class Main {
         // 4. 숫자를 잘못 입력 -> 한글 , 영어 , 자리수 12자리 아님
         // 5. 종료하는 방법
         // 6. 기존에 파일이 존재하면 처리는 어떻게 ?
+
+        //다운로드 받은 poi 파일 주소 : https://archive.apache.org/dist/poi/release/bin/
+        //참고한 블로그 글 : https://yangsosolife.tistory.com/7  , https://yangsosolife.tistory.com/8
+
     }
 }
