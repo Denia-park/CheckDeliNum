7b5583874782f868a12479234a808774bfbab53d
Hyunggi <phg9587@naver.com>
2022-06-27 20:51:11 +0900

    Add : 기본 파일 생성

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        // 1. 중복되는 경우 - 송장
+        // 2. 중복되는 경우 - csv
+        // 3. 해당 데이터가 없는 경우
+        // 4. 숫자를 잘못 입력 -> 한글 , 영어 , 자리수 12자리 아님
+        // 5. 종료하는 방법
+        // 6. 기존에 파일이 존재하면 처리는 어떻게 ?
+    }
+}
